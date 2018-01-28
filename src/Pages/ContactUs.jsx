1cd117f89f7b077c4227ef57a5433b81b9d8da81
JoshRamen <joshuarajanm@gmail.com>
2018-01-27 21:58:38 -0500

    CSS edits, link fixing.

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -4,14 +4,20 @@ import FBlogo from '../Assets/facebooklogo.png';
 import TWlogo from '../Assets/twitterlogo.png';
 
 export default () => (
-    <div className="contactUs"> 
-        <h1 className="contactHeader">Contact Us</h1>
-        <p className="contactParagraph">If you would like to reach out to us in order to ask us for anything regarding Dynami, email us at:</p>
-        <h2 className="emailSubheader">dynamiproject@gmail.com</h2>
-        <p className="contactParagraph">Add us on social media!</p>
-        <div className = "imageDiv">
-        <   img src={FBlogo} className="FBlogo" alt="facebook logo" />
-        <   img src={TWlogo} className="TWlogo" alt="twitter logo" />
+    <body className="contactBody">
+        <div className="contactUs"> 
+            <h1 className="contactHeader">Contact Us</h1>
+            <p className="contactParagraph">If you would like to reach out to us in order to ask us for anything regarding Dynami, email us at:</p>
+            <h2 className="emailSubheader">dynamiproject@gmail.com</h2>
+            <p className="contactParagraph">Add us on social media!</p>
+            <div className = "imageDiv">
+                <a href="https://www.facebook.com/Dynami-Project-1002872376531590/">
+                <   img src={FBlogo} className="FBlogo" alt="facebook logo" />
+                </a>
+                <a href="https://twitter.com/ProjectDynami">
+                <   img src={TWlogo} className="TWlogo" alt="twitter logo" />
+                </a>
+            </div>
         </div>
-    </div>
+    </body>
 );
