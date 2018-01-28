65ad7524b2f9842eb813fe6494ad4fe227fa683c
JoshRamen <joshuarajanm@gmail.com>
2018-01-27 22:57:05 -0500

    Resources preliminary HTML and CSS before getting the REACT part done.

--- src/Pages/Resources.jsx
+++ src/Pages/Resources.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import './Resources.css';
 export default () => (
-    <div> 
+    <div className = "resources"> 
         <h1>Resources </h1>
-        <p className = "locationsText">Currently, we have locations in Ohio, New York, and Pennsylvania, where anyone can come and receive the help that they need. Our therapists will be available during hours to listen to your situation, offer advice, and more. We are always looking to expand and offer our locations across the nation.</p>
+        <p className = "locationsText">Currently, we have locations in Ohio, New York, and Pennsylvania, where anyone can come and receive the help that they need. Our therapists will be available during hours to listen to your situation, offer advice, and talk to you. We are always looking to expand and offer our locations across the nation, and will update our database as more locations are created.</p>
+        <h1>Testimonials</h1>
+        <h3>Many people have been aided through Dynami:</h3>
+        <p className = "testimonialReqText">If you would like to submit a testimonial of your own, feel free to do so below! If you don't want to provide your name, feel free to leave it as "Anonymous".</p>
     </div>
 );
