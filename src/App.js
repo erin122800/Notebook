c09fdbb8bf0e98126c88fcfa44d68a625b73bc64
erin122800 <erinlaxton122800@gmail.com>
2018-01-27 19:51:51 -0500

    Used the pages.

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Dynami</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AboutUs/>
+        <Resources/>
+        <ContactUs/>
       </div>
     );
   }
