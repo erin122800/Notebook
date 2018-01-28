d4ce98a5a331a86e9abd842c4dab89873fc9326d
JoshRamen <joshuarajanm@gmail.com>
2018-01-27 22:16:03 -0500

    First rudimentary edits to Resources, nothing much.

--- src/Pages/Resources.jsx
+++ src/Pages/Resources.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import './Resources.css';
 export default () => (
     <div> 
         <h1>Resources </h1>
-        <p> </p>
+        <p className = "locationsText">Currently, we have locations in Ohio, New York, and Pennsylvania, where anyone can come and receive the help that they need. Our therapists will be available during hours to listen to your situation, offer advice, and more. We are always looking to expand and offer our locations across the nation.</p>
     </div>
 );
