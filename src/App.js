fb6f198ebb9bb002aefa0b568880ff970eff940f
erin122800 <erinlaxton122800@gmail.com>
2018-01-27 23:06:31 -0500

    Good stuff

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import NavBar from './Components/NavBar';
 import AboutUs from "./Pages/AboutUs";
 import ContactUs from "./Pages/ContactUs";
 import Resources from "./Pages/Resources";
+import ScrollToTop from 'react-scroll-up';
+import Arrow from './Assets/arrow-circle-top-4x.png';
 
 const pages = [
-  {name: 'About Us', component: AboutUs },
-  {name: 'Resources', component: Resources },
-  {name: 'Contact Us', component: ContactUs },
+  { name: 'About Us', component: AboutUs },
+  { name: 'Resources', component: Resources },
+  { name: 'Contact Us', component: ContactUs },
 ];
 
 
@@ -24,36 +26,39 @@ class App extends Component {
     this.navigate = this.navigate.bind(this);
   }
   navigate(page) {
-    this.setState({page});
+    this.setState({ page });
   }
   render() {
     const Page = this.state.page;
     return (
       <div className="App">
         <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Dynami</h1>
-          
+
         </header>
-        <NavBar pages={pages} onChange={this.navigate}/>
+        <NavBar pages={pages} onChange={this.navigate} />
         <Page />
         <footer>
           <div>
-            <a href = "https://twitter.com/ProjectDynami?lang=en"> 
-            <p> @ProjectDynami on Twitter</p>
+            <a href="https://twitter.com/ProjectDynami?lang=en">
+              <p> @ProjectDynami on Twitter</p>
             </a>
-            <a href = "https://www.facebook.com/Dynami-Project-1002872376531590/">
-            <p> "Project Dynami" on Facebook</p>
+            <a href="https://www.facebook.com/Dynami-Project-1002872376531590/">
+              <p> "Project Dynami" on Facebook</p>
             </a>
-           </div>
-            <div>
-              <p>&copy; 2018 </p>
-            </div>
-              <div>
+            <a>
+              <p> &copy; 2018 </p>
+            </a>
+          </div>
+          <div>
             <img src={logo} className="footer-logo" alt="logo" />
-             </div>
+          </div>
         </footer>
-      </div> 
+        <ScrollToTop showUnder={160}>
+  <img src={Arrow} />
+</ScrollToTop>
+      </div>
     );
   }
 }
