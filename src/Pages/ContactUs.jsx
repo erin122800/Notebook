710a605efc4ce85cbca630d4d95e937dfc051637
erin122800 <erinlaxton122800@gmail.com>
2018-01-28 11:15:16 -0500

    Added team picture to ContactUs page,

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -3,11 +3,12 @@ import './ContactUs.css';
 import FBlogo from '../Assets/facebooklogo.png';
 import TWlogo from '../Assets/twitterlogo.png';
 import ContactUsBG1 from '../Assets/ContactUsBG1.jpg';
+import TeamPic from "../Assets/TeamPic.JPG";
 
 export default () => (
     <body className="contactBody">
         <div className="contactUs">
-            <img src = {ContactUsBG1} alt = "contact us"/>
+            <img src = {ContactUsBG1} alt = "contact us" className = "ContactPic"/>
             <h1 className="contactHeader">Contact Us</h1>
             <p className="contactParagraph">We are Team 6 from the 2018 Codethon. If you would like to reach out to us in order to ask us for anything regarding Dynami, email us at:</p>
             <h2 className="emailSubheader">dynamiproject@gmail.com</h2>
@@ -20,6 +21,7 @@ export default () => (
                 <   img src={TWlogo} className="TWlogo" alt="twitter logo" />
                 </a>
             </div>
+            <img src = {TeamPic} alt = "TeamPic" className = "TeamPic"/>
         </div>
     </body>
 );
