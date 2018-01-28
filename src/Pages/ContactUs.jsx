b302e56d20393489d943d65bc44d39861e51b285
erin122800 <erinlaxton122800@gmail.com>
2018-01-28 10:27:34 -0500

    contact us team 6

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -9,7 +9,7 @@ export default () => (
         <div className="contactUs">
             <img src = {ContactUsBG1} alt = "contact us"/>
             <h1 className="contactHeader">Contact Us</h1>
-            <p className="contactParagraph">If you would like to reach out to us in order to ask us for anything regarding Dynami, email us at:</p>
+            <p className="contactParagraph">We are Team 6 from the 2018 Codethon. If you would like to reach out to us in order to ask us for anything regarding Dynami, email us at:</p>
             <h2 className="emailSubheader">dynamiproject@gmail.com</h2>
             <p className="contactParagraph">Add us on social media!</p>
             <div className = "imageDiv">
