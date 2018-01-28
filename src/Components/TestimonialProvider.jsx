c56c984f1a8fafffd7b98bd03f62f3c98920b0e0
erin122800 <erinlaxton122800@gmail.com>
2018-01-28 00:26:31 -0500

    Testimonies

--- src/Components/TestimonialProvider.jsx
+++ src/Components/TestimonialProvider.jsx
@@ -2,9 +2,29 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 async function getData(uri) {
+    var response = await fetch ("https://wctd2018-a26c.restdb.io/rest/testimonials", {
+    headers: new Headers({
+        "content-type": "application/json",
+        "x-apikey": "5a6cf7b27d7ef24c5cf08fe7",
+        "cache-control": "no-cache"
+      })
+    });
+    var data = await response.json();
+    return data;
 }
 
 async function uploadData(uri, data) {
+    var response = await fetch ("https://wctd2018-a26c.restdb.io/rest/testimonials", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: new Headers({
+            "content-type": "application/json",
+            "x-apikey": "5a6cf7b27d7ef24c5cf08fe7",
+            "cache-control": "no-cache"
+          })
+        });
+        var data = await response.json();
+        return data;
 }
 
 const API = {
