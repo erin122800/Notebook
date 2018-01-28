3353bb1a58f53007242e53ec30544f437b41c34c
JoshRamen <joshuarajanm@gmail.com>
2018-01-27 21:22:09 -0500

    Development of Contact Us page

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import './ContactUs.css';
+import FBlogo from './src/Assets/facebooklogo.png';
 
 export default () => (
-    <div> 
-        <h1>Contact Us</h1>
-        <p> </p>
+    <div className="contactUs"> 
+        <h1 className="contactHeader">Contact Us</h1>
+        <p className="contactParagraph">If you would like to reach out to us in order to ask us for anything regarding Dynami, email us at:</p>
+        <h2 className="emailSubheader">dynamiproject@gmail.com</h2>
+        <p className="contactParagraph">Add us on social media!</p>
+        <div className = "imageDiv">
+        <   img src={FBlogo} className="FBlogo" alt="facebook logo" />
+        </div>
     </div>
 );
