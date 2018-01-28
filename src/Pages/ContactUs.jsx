bc5dc72939817122efe88ffdecad38a7ce032fb9
JoshRamen <joshuarajanm@gmail.com>
2018-01-28 09:33:54 -0500

    contactus image given class name

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -7,7 +7,7 @@ import ContactUsBG1 from '../Assets/ContactUsBG1.jpg';
 export default () => (
     <body className="contactBody">
         <div className="contactUs">
-            <img src = {ContactUsBG1} style={{Height: 300}} alt = "contact us"/>
+            <img className = "contactImage" src = {ContactUsBG1} alt = "contact us"/>
             <h1 className="contactHeader">Contact Us</h1>
             <p className="contactParagraph">If you would like to reach out to us in order to ask us for anything regarding Dynami, email us at:</p>
             <h2 className="emailSubheader">dynamiproject@gmail.com</h2>
