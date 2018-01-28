7c027dc2affb089bc020b510516eb273ac083542
JoshRamen <joshuarajanm@gmail.com>
2018-01-28 09:19:15 -0500

    contactus remove parallax

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import './ContactUs.css';
 import FBlogo from '../Assets/facebooklogo.png';
 import TWlogo from '../Assets/twitterlogo.png';
-import Parallax from 'react-lazy-parallax';
 import ContactUsBG1 from '../Assets/ContactUsBG1.jpg';
 
 export default () => (
     <body className="contactBody">
         <div className="contactUs">
-            <img src = {ContactUsBG1} style={{Height: 300}} />
+            <img src = {ContactUsBG1} style={{Height: 300}} alt = "contact us"/>
             <h1 className="contactHeader">Contact Us</h1>
             <p className="contactParagraph">If you would like to reach out to us in order to ask us for anything regarding Dynami, email us at:</p>
             <h2 className="emailSubheader">dynamiproject@gmail.com</h2>
